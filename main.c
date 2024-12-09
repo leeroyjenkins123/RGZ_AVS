d985a3af86cda4fb1b363ad622a208509cc488a0
PXLXTA <124140129+leeroyjenkins123@users.noreply.github.com>
2024-12-09 14:58:58 +0700

    Update main.c

--- main.c
+++ main.c
@@ -7,10 +7,10 @@ int main()
       // This line will be executed first
     printf("Program to sleep for 10 second in Linux.\n");
 
-    sleep(10);
+    sleep(100);
     // after 10 seconds this next line will be executed.
   
-    printf("This line will be executed after 10 second.");
+    printf("This line will be executed after 10 second.\n");
 
     return 0;
 }
