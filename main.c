0464dfa597d9a95186fba06797fbcf700c83a49b
PXLXTA <124140129+leeroyjenkins123@users.noreply.github.com>
2024-12-09 15:05:34 +0700

    Update main.c

--- main.c
+++ main.c
@@ -4,13 +4,7 @@
 int main()
 {
 
-      // This line will be executed first
-    printf("Program to sleep for 10 second in Linux.\n");
-
-    sleep(100);
-    // after 10 seconds this next line will be executed.
-  
-    printf("This line will be executed after 10 second.\n");
+    printf("Zdarova zebal");
 
     return 0;
 }
