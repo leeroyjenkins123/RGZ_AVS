3570e25d89cafae518f2f69206f0224e37e75578
PXLXTA <124140129+leeroyjenkins123@users.noreply.github.com>
2024-12-09 14:46:21 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,12 +7,7 @@ int main() {
     cout << "Hello world!" << endl;
 
     // Ожидание ввода пользователя с помощью цикла
-    cout << "Press any key to exit..." << endl;
-
-    // Бесконечный цикл, ожидающий вручную завершения
-    while (true) {
-        this_thread::sleep_for(chrono::seconds(1)); // Задержка 1 секунда
-    }
+    getchar();
 
     return 0;
 }
