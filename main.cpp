276eaf9190fd2b3487a2f71026dc5d48a2f91ddd
PXLXTA <124140129+leeroyjenkins123@users.noreply.github.com>
2024-12-09 14:37:35 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <cstdlib>  // Для system("pause")
 using namespace std;
 
-int main(){
-  cout<<"Hello world!"<<endl;
-  return 0;
+int main() {
+    cout << "Hello world!" << endl;
+
+    // Ожидание ввода пользователя
+    cout << "Press any key to exit..." << endl;
+
+    // Используем system("pause") для Windows или getchar() для Linux
+    #if defined(_WIN32) || defined(_WIN64)
+        system("pause");  // Ожидает нажатия клавиши в Windows
+    #else
+        getchar();  // Ожидает ввода в Linux/Unix системах
+    #endif
+
+    return 0;
 }
